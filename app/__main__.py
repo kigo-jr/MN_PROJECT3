696eaecdacea8d8968332e092f0a693686af3fed
Jakub Kluk <kuba.kluk85@gmail.com>
2022-06-04 18:14:27 +0200

    added method for plotting without interpolation

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,8 @@
-from csv_utils.functionality import cubic_spline_data_plot, lagrange_data_plot
+from csv_utils.functionality import cubic_spline_data_plot, lagrange_data_plot, plot_data
+
+plot_data("./data/WielkiKanionKolorado.csv")
+plot_data("./data/MountEverest.csv")
+plot_data("./data/SpacerniakGdansk.csv")
 
 lagrange_data_plot("./data/WielkiKanionKolorado.csv", 5)
 lagrange_data_plot("./data/MountEverest.csv", 5)
